e1699b2e5c87669679f7bf8a4907903509481074
asif7480 <a.a.khan7480@gmail.com>
2025-11-06 10:41:34 +0500

    Integrated user, dashboard and auth templates

---
+++ VehicleInsuranceManagement/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleInsuranceManagement.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VehicleInsuranceManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleInsuranceManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
